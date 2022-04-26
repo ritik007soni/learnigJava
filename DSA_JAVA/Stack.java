ac83dea7dc8925036c091a256761f66587a1dc1d
Ritik <soniritik105@gmail.com>
2022-04-26 20:03:27 +0530

    Stack implementation using array

---
+++ DSA_JAVA/Stack.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Stack{
+
+    static int top = 0;
+    static int size = 100;
+    static int num=0;
+    static Object[] St = new Object[size];
+
+
+    Stack(){
+        System.out.println("Enter the size of stack");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+    }
+    
+
+    void push(Object[] St,int ele) throws Exception{
+        if(top>=num){
+            throw new Exception("Stack overflow");
+        }
+        else{       
+            top++;
+            St[top] = ele;
+        }
+    }
+
+    Object pop(Object[] St) throws Exception{
+        if(top==0){
+            throw new Exception("Stack is Empty");
+        }
+        else{
+            Object x = St[top];
+            St[top] = null;
+            top--;
+            return x;
+        }
+    }
+
+    void peek(Object[] St) throws Exception{
+        int t = top;
+        while(t>0){
+            System.out.println(St[t]);
+            t--;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Stack obj = new Stack();
+
+
+        try{
+            while(true){
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Press 1 to push\n Press 2 to pop\n Press 3 to peek\n Press 0 to exit: \n");
+                int opt = sc.nextInt();
+                if(opt == 1){
+                    System.out.println("Enter the elemnt to push");
+                    int ele = sc.nextInt();
+                    obj.push(St,ele);
+                }
+                else if(opt == 2){
+                    Object x = obj.pop(St);
+                    System.out.println("Poped element is"+x);
+                }
+                else if(opt == 3){
+                    obj.peek(St);
+                }
+                else{
+                    break;
+                }
+
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
